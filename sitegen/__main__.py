836148b739231ad4003705230ab031d13d46a73f
itsdanjc <dan@itsdanjc.com>
2025-12-26 17:57:16 +0000

    Started CLI

--- sitegen/__main__.py
+++ sitegen/__main__.py
@@ -1,1 +1,19 @@
-print("Hello, World!")
+import click, logging
+from .log import configure_logging
+
+logger = logging.getLogger(__name__)
+
+
+@click.group()
+@click.option('--verbose', '-v', is_flag=True, default=False)
+def cli(verbose: bool):
+    configure_logging(verbose)
+
+
+@cli.command(help="Build the site.")
+def build():
+    logger.info("Hello world")
+
+
+if __name__ == "__main__":
+    cli()
